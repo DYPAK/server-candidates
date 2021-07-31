785a677b0f9f43ff19f1957ce50a97de5229b9ce
DYPAK <dypakin@gmail.com>
2021-07-31 11:35:19 +0400

    add Search (POST) and Output (GET) candidate for /Candidate/index

--- server/application/controller/controller_candidate.php
+++ server/application/controller/controller_candidate.php
@@ -11,16 +11,53 @@ class Controller_Candidate extends Controller
 
     function action_index()
     {
+        $output = false;
         $_POST = (array)json_decode(file_get_contents('php://input'), TRUE);
 
-        if (isset($_POST['id'])) {
-            echo json_encode("rrr");
+        if (isset($_POST['updateCandidate'])) {
+
+            $output = false;
+            $id = htmlspecialchars(trim($_POST['updateCandidate']['id']));
+            $name = htmlspecialchars(trim($_POST['updateCandidate']['name']));
+            $date = htmlspecialchars(trim($_POST['updateCandidate']['date']));
+            $description = htmlspecialchars(trim($_POST['updateCandidate']['description']));
+            $technology = null; $i = 0;
+            foreach ($_POST['updateCandidate']['technologies'] as $value)
+            {
+                $technology[$i] = htmlspecialchars(trim($value));
+                $i++;
+            }
+            if ($technology != null) {
+
+            }
+
+        }
+        if (isset($_POST['page'])) {
+            $page =  htmlspecialchars(trim($_POST['page']));
+            $this->model->checkSelector($_POST['page']);
+        }
+        if (isset($_POST['searchCandidates'])) {
+            $name = htmlspecialchars(trim($_POST['searchCandidates']['name']));
+            $dateStart = htmlspecialchars(trim($_POST['searchCandidates']['dateStart']));
+            $dateEnd = htmlspecialchars(trim($_POST['searchCandidates']['dateEnd']));
+            $i=0;
+            $technology = [];
+            foreach ($_POST['searchCandidates']['technologies'] as $value)
+            {
+                $technology[$i] = htmlspecialchars(trim($value));
+                $i++;
+            }
+            $allTechnologies = $this->model->getAllTechnologies();
+            $mas = $this->model->getAllCandidates($technology,$name,$dateStart,$dateEnd);
+            $output = $this->model->sortCandidates($mas,$allTechnologies,self::CANDIDATES);
         }
         else {
+
             $allTechnologies = $this->model->getAllTechnologies();
-            $mas = $this->model->getAllCandidates($allTechnologies);
-            $candidates = $this->model->sortCandidates($mas,$allTechnologies,self::CANDIDATES);
-            echo json_encode($candidates);
+            $mas = $this->model->getAllCandidates();
+            $output = $this->model->sortCandidates($mas,$allTechnologies,self::CANDIDATES);
+
         }
+        echo json_encode($output);
     }
 }
--- server/application/models/model_candidate.php
+++ server/application/models/model_candidate.php
@@ -2,7 +2,11 @@
 
 class Model_Candidate extends Model
 {
-
+    /**
+     * function take
+     * in bd mas technologies
+     * @return array
+     */
     function getAllTechnologies()
     {
         $sql = "SELECT technology FROM `technologies`";
@@ -19,6 +23,16 @@ class Model_Candidate extends Model
         return $result;
     }
 
+
+    /**
+     * function search and return
+     * mas witch candidates
+     * @param array $technologiesCheck
+     * @param string $name
+     * @param string $dateStart
+     * @param string $dateEnd
+     * @return array
+     */
     function getAllCandidates($technologiesCheck=[], $name="", $dateStart="0001-01-01", $dateEnd="9999-12-31") {
 
         $sql = 'SELECT id_candidates, full_name, date_of_birth, description, technology, skill  FROM `candidates` can JOIN `connect` c ON can.id = c.id_candidates JOIN `technologies` t ON c.id_technologies = t.id '
@@ -30,17 +44,26 @@ class Model_Candidate extends Model
             $sql .=" AND (technology = ? )";
             $params[3] = $technologiesCheck[0];
             for($i = 1; $i < count($technologiesCheck); $i++ ) {
-                $sql .=" OR (technology = ? )";
+                $sql .=" AND (technology = ? )";
                 $params[$i+3] = $technologiesCheck[$i];
             }
         }
         $sql .= "ORDER BY can.id";
         $query = $this->connect->prepare($sql);
-            $query ->execute($params);
+        $query ->execute($params);
         return $query -> fetchAll(PDO::FETCH_CLASS);
 
     }
 
+    /**
+     * function create
+     * answer user
+     * @param $allCandidates
+     * @param $technologiesBase
+     * @param $candidates_one_page
+     * @param int $page
+     * @return array
+     */
     function sortCandidates($allCandidates, $technologiesBase, $candidates_one_page, $page=1) {
         $cand= [];
         $technologies = [];
@@ -48,24 +71,53 @@ class Model_Candidate extends Model
             foreach($technologiesBase as $technologyBase) {
                 $technologies[$technologyBase] = 0;
             }
-        $temp_2 = -1; $temp_1 = $allCandidates[0]->id_candidates + 1;
+        $j = -1; $temp_1 = $allCandidates[0]->id_candidates + 1;
         $result['technologies'] = $technologiesBase;
         for ($i = 0; $i < count($allCandidates); $i++) {
             if ($temp_1 != $allCandidates[$i] ->id_candidates) {
-                $temp_2++;
-                $cand[$temp_2]['id'] = $allCandidates[$i] -> id_candidates;
-                $cand[$temp_2]['name'] = $allCandidates[$i] -> full_name;
-                $cand[$temp_2]['date'] = $allCandidates[$i] -> date_of_birth;
-                $cand[$temp_2]['description'] = $allCandidates[$i] -> description;
-                $cand[$temp_2]['technology'] = $technologies;
+                $j++;
+                $cand[$j]['number'] = $j+1;
+                $cand[$j]['id'] = $allCandidates[$i] -> id_candidates;
+                $cand[$j]['name'] = $allCandidates[$i] -> full_name;
+                $cand[$j]['date'] = $allCandidates[$i] -> date_of_birth;
+                $cand[$j]['description'] = $allCandidates[$i] -> description;
+                $cand[$j]['technology'] = $technologies;
                 $temp_1 = $allCandidates[$i] ->id_candidates;
             }
-            $cand[$temp_2]['technology'][$allCandidates[$i] ->technology] = $allCandidates[$i] ->skill;
+            $cand[$j]['technology'][$allCandidates[$i] ->technology] = $allCandidates[$i] ->skill;
 
         }
-        $result['maxPage'] = ++$temp_2;
+        $j = 0; $i = $page-1; $candidates_one_page = $candidates_one_page + $i;
+        while (($i < $candidates_one_page) && ($cand[$i] == [])) {
+            $result['candidate'][$j] = $cand[$i];
+            $i++; $j++;
+        }
+        $result['maxPage'] = ceil(count($cand)/$candidates_one_page);
+        ($result['maxPage'] == 0) ? $result['maxPage'] = 1 : 0;
         $result['candidate'] = $cand;
         return $result;
     }
+
+//    function UpdateCandidate($id, $name, $date, $description, $technologies) {
+//        $sql = 'SELECT id_candidates, full_name, date_of_birth, description, technology, skill  FROM `candidates` can JOIN `connect` c ON can.id = c.id_candidates JOIN `technologies` t ON c.id_technologies = t.id '
+//            .'WHERE (full_name LIKE ? ) AND (date_of_birth >= ?) AND (date_of_birth <= ?)';
+//
+//        //  $params = [0 => "%$name%", 1 => $dateStart, 2 => $dateEnd];
+//
+//    }
+//
+//    function checkSelector($selector, $number_page, $max_page)
+//    {
+//        if (preg_match("/(-)|(\+)/",$selector)) {
+//            ($selector == "+")? $selector = ++$number_page : $selector = --$number_page;
+//        }
+//        if (preg_match("/\d+/",$selector)) {
+//            ($selector > $max_page) ? $selector = $max_page : 0 ;
+//            ($selector < 1) ? $selector = 1 : 0 ;
+//            return (int)$selector;
+//        }
+//        return $number_page;
+//    }
+
 
 }
