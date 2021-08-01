b71aecf06fb4c507621f167f3e715eeddd0b54cb
DYPAK <56132160+DYPAK@users.noreply.github.com>
2021-08-01 20:54:52 +0400

    Update controller_authorization.php

--- server/application/controller/controller_authorization.php
+++ server/application/controller/controller_authorization.php
@@ -9,10 +9,6 @@ class Controller_Authorization extends Controller
 
     function action_index()
     {
-<<<<<<< HEAD
-=======
-
->>>>>>> 785a677b0f9f43ff19f1957ce50a97de5229b9ce
         $_POST = (array)json_decode(file_get_contents('php://input'), TRUE);
 
         if (isset($_POST['login'])) {
@@ -20,23 +16,18 @@ class Controller_Authorization extends Controller
             $login = trim($_POST['login']);
             $password = trim($_POST['password']);
 
-<<<<<<< HEAD
-=======
             $login = htmlspecialchars($login);
             $password = htmlspecialchars($password);
 
->>>>>>> 785a677b0f9f43ff19f1957ce50a97de5229b9ce
             $key = $this->model->authorizationUser($login, $password);
             if (isset($key))
                 echo json_encode($key);
         }
 
-<<<<<<< HEAD
-=======
         if($_GET['page'] != NULL) {
             echo json_encode("lol");
         }
->>>>>>> 785a677b0f9f43ff19f1957ce50a97de5229b9ce
+       
     }
 }
 
