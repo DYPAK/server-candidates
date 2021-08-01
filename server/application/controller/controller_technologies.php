c801b810c9aea733bb3c7e39d3678ba67938d08d
DYPAK <56132160+DYPAK@users.noreply.github.com>
2021-08-01 21:03:37 +0400

    Update controller_technologies.php

--- server/application/controller/controller_technologies.php
+++ server/application/controller/controller_technologies.php
@@ -12,19 +12,5 @@ class Controller_Technologies extends Controller
     {
         $data = $this->model->getAllTechnologies();
     }
-=======
-<?php
 
-class Controller_Technologies extends Controller
-{
-    public function __construct()
-    {
-        $this->model = new Model_Technologies();
-    }
-
-    function action_index()
-    {
-        $data = $this->model->getAllTechnologies();
-    }
->>>>>>> 785a677b0f9f43ff19f1957ce50a97de5229b9ce
 }
