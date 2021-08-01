2faa503aa6c860ad550ad46da91a6cc53609f45c
DYPAK <56132160+DYPAK@users.noreply.github.com>
2021-08-01 21:02:31 +0400

    Update controller_registration.php

--- server/application/controller/controller_registration.php
+++ server/application/controller/controller_registration.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 class Controller_Registration extends Controller
@@ -23,25 +22,4 @@ class Controller_Registration extends Controller
             }
         }
     }
-=======
-<?php
-
-class Controller_Registration extends Controller
-{
-    public function __construct()
-    {
-        $this->model = new Model_Registration();
-    }
-
-    function action_index()
-    {
-        $_POST = (array)json_decode(file_get_contents('php://input'), TRUE);
-
-        if (isset($_POST['full_name'])) {
-            $key = $this->model->registrationUser($_POST['full_name'], $_POST['login'], $_POST['email'], $_POST['password'], $_POST['password_confirm']);
-            echo json_encode($key);
-        }
-
-    }
->>>>>>> 785a677b0f9f43ff19f1957ce50a97de5229b9ce
 }
