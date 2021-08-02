db5faa70c132d9bff6a2081de9956c415d6f98a9
DYPAK <56132160+DYPAK@users.noreply.github.com>
2021-08-02 06:10:43 +0400

    Update model_registration.php

--- server/application/models/model_registration.php
+++ server/application/models/model_registration.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 class Model_Registration extends Model
@@ -28,25 +27,4 @@ class Model_Registration extends Model
         }
         return $key;
     }
-=======
-<?php
-
-class Model_Registration extends Model
-{
-    function registrationUser($full_name, $login, $email, $password, $password_confirm) {
-
-      if($full_name != NULL && $login != NULL && $email != NULL && $password != NULL && $password_confirm != NULL) {
-        if($password == $password_confirm) {
-            $password = md5($password);
-            mysqli_query($this->connect, "INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`) VALUES (NULL, '$full_name', '$login', '$email', '$password')");
-            //header('Location: ../../Authorization/index');
-            return true;
-
-        } else {
-          //header('Location: ../../Registration/index');
-            return false;
-        }
-      }
-    }
->>>>>>> 785a677b0f9f43ff19f1957ce50a97de5229b9ce
 }
